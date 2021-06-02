2a3cbfcec5b87800d55ce7f0a25bbcb58f8f97c0
Sixsixfive <1615939217@qq.com>
2021-06-02 14:54:11 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "parser": "vue-eslint-parser",
+    "parserOptions": {
+        "parser": "babel-eslint",
+        "ecmaVersion": 2015,
+        "sourceType": "module"
+    },
+    "globals": {
+        document: true,
+        window: true
+    },
+    "rules": {
+        // allow async-await
+        'generator-star-spacing': 'off',
+        // allow debugger during development
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        /*
+	    0 或’off’：  关闭规则。
+	    1 或’warn’： 打开规则，并且作为一个警告，字体颜色为黄色（并不会导致检查不通过）。
+	    2 或’error’：打开规则，并且作为一个错误 ，色体颜色为红色(退出码为1，检查不通过)。
+        */
+        // 以下为该项目自定义部分
+        "semi": 0,    //语句强制分号结尾
+        'indent': [2, 2], //缩进风格 - 开启缩进4格
+        'no-spaced-func': 2, //函数调用时 函数名与()之间不能有空格 - 开启
+        'no-const-assign': 2, //禁止修改const声明的变量 - 开启
+        'space-before-function-paren': [2, 'always'], //函数定义时括号前面要有空格 - 关闭
+        'eol-last': 2, //文件以单一的换行符结束 - 关闭
+        'camelcase': 2, //强制驼峰法命名 - 关闭
+        'no-undef': 2, //不能有未定义的变量 - 关闭
+        'no-alert': 2, //禁止使用alert confirm prompt - 关闭
+        'arrow-parens': 0, //箭头函数用小括号括起来 - 关闭
+        "strict": 2,//使用严格模式
+        "eqeqeq": 2,//必须使用全等
+    }
+}
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div>row one</div>
+    <slot name="first" propsValue="子组件的值"></slot>
+    <div>row two</div>
+    <slot name="second"></slot>
+    <div>row three</div>
+    <slot name="third"></slot>
+    <div>row four</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  methods: {
+    XiuxiuXiu () {},
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
